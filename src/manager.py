79cf0d5f14bdc0b43a9aee1d59f2b881e4a1a30c
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2025-02-13 11:28:58 +0100

    add validator for redis server connection

--- src/manager.py
+++ src/manager.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass, field
 from random import randint
+import redis
+import sys
 
 from IPython.display import Markdown, display
 from .graphs.graph import build_graph,  CompiledStateGraph
 from .graphs.graph_only_researchers import build_graph as build_graph_only_researchers
 from .states.state import State
+from .cache.redis_cache import RedisCache
 
 from langgraph.types import Command
 
@@ -18,6 +21,26 @@ class Manager:
         default_factory=build_graph_only_researchers
     )
 
+    def __post_init__(self):
+        """Check if the Redis cache is available right after initialization."""
+        self._check_redis_connection()
+    
+    def _check_redis_connection(self):
+        """
+        Attempts to ping the Redis cache.
+        If the ping fails, instructs the user to install Redis and run `redis-server` first.
+        """
+        try:
+            cache = RedisCache()
+            cache.client.ping()
+        except redis.exceptions.ConnectionError:
+            error_message = (
+                "Redis server is not running. Please install Redis "
+                "(https://redis.io/docs/latest/operate/oss_and_stack/install/install-redis/) "
+                "and run `redis-server` first."
+            )
+            raise RuntimeError(error_message)
+    
     def invoke(
             self, 
             inputs: dict, 
